ae291c33b2b4502c130f27444e089a274c6d2f56
PacocaApps <gpaaadm@gmail.com>
2020-02-16 17:52:06 -0300

    sec

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function checksecurity(){
-    var txt = prompt('PASSWORD'); if(txt === 'hnpth3vb'){window.open('Control69/index.html')}else{}
+    var txt = prompt('PASSWORD'); if(txt === 'Hnpth3vb'){window.open('Control69/index.html')}else{}
 
 
 }
