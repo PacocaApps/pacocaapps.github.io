3c02d919d53d28a8cc84761b115aa644836240ef
PacocaApps <gpaaadm@gmail.com>
2020-03-31 20:04:57 -0300

    rr

--- Control69/trial.js
+++ Control69/trial.js
@@ -13,23 +13,23 @@ var password = prompt("Password")
 if(password === "hnpth3vb"){
 window.open("https://pacocaapps.github.io/Control69/control.html")
 }else{
-
+window.close();
 
 }
 
 
 
 }else{
-
+    window.close();
 }
 
 
 }
 function god(){
 correct = 0;
-
+window.close();
 
 } function phallus(){
 correct = 0;
-
+window.close();
 }
