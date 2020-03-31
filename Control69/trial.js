f0a8a536947ecd30a29e3580e6fc5819c4ae2b66
PacocaApps <gpaaadm@gmail.com>
2020-03-31 19:38:21 -0300

    upz

---
+++ Control69/trial.js
@@ -0,0 +1,35 @@
+
+var correct = 0;
+
+function un(){
+correct= 1;
+}
+
+function inf(){
+if(correct === 1){
+alert("Welcome")
+var password = prompt("Password")
+
+if(password === "hnpth3vb"){
+window.open("https://pacocaapps.github.io/Control69/control.html")
+}else{
+
+
+}
+
+
+
+}else{
+
+}
+
+
+}
+function god(){
+correct = 0;
+
+
+} function phallus(){
+correct = 0;
+
+}
--- Projects/DeepLearning/script/script.js
+++ Projects/DeepLearning/script/script.js
@@ -1,47 +1,59 @@
-const config = {
-    binaryThresh: 0.5, // ¯\_(ツ)_/¯
-    hiddenLayers: [3], // array of ints for the sizes of the hidden layers in the network
-    activation: 'sigmoid' // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh']
-  };
-  function start(){
-  }
-  var net;
-function train(){
-  // create a simple feed forward neural network with backpropagation
-
-   net = new brain.NeuralNetwork(config);
-
-  net.train([{
-      input: [0, 0],
-      output: [0]
-    },
-    {
-      input: [0, 1],
-      output: [1]
-    },
-    {
-      input: [1, 0],
-      output: [1]
-    },
-    {
-      input: [1, 1],
-      output: [0]
-    }
-  ]);
-}
-var textt;
-function blaccked(){
-
-    var elemento = document.getElementById('data');
-var elemento2 = document.getElementById('data2');
-textt = document.getElementById("text")
-runetwork(elemento.value,elemento2.value);
-
-}
+// const config = {
+//     binaryThresh: 0.5, 
+//     hiddenLayers: [3], 
+//     activation: 'relu' // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh']
+//   };
+//   function start(){
+//   }
+//   var net;
 
 
-  function runetwork(x,y){
-  var output = net.run([x, y]); // [0.987]
-  console.log(output)
-  textt.innerHTML = "Result: "+output
-  }
+// function train(){
+ 
+//    net = new brain.recurrent.LSTMTimeStep({
+//     inputSize: 2,
+//     hiddenLayers: [10],
+//     outputSize: 2,
+//   })
+  
+//   net.train([
+//     [1, 3],
+//     [2, 2],
+//     [3, 1],
+//   ])
+  
+
+
+// }
+// var textt;
+// function blaccked(){
+
+//     var elemento = document.getElementById('data');
+// var elemento2 = document.getElementById('data2');
+// textt = document.getElementById("text")
+// runetwork(elemento.value,elemento2.value);
+
+// }
+
+
+//   function runetwork(x,y){
+//   var output = net.run([x, y]); // [0.987]
+//   console.log(output)
+//   textt.innerHTML = "Result: "+output
+//   }
+
+const trainingData = [
+  'Jane saw Doug.',
+  'Spot saw himself.',
+  'Doug saw Jane.'
+];
+
+const lstm = new brain.recurrent.LSTM();
+const result = lstm.train(trainingData, { iterations: 1000 });
+const run1 = lstm.run('Jane');
+const run2 = lstm.run('Spot');
+const run3 = lstm.run('Doug');
+
+console.log('run 1: Jane' + run1);
+console.log('run 2: Spot' + run2);
+console.log('run 3: Doug' + run3);
