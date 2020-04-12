6705e53c5c285c68ed2b36352b1cfab1028a2b3f
PacocaApps <gpaaadm@gmail.com>
2020-04-11 21:25:26 -0300

    deas sss

--- fotos/trial.js
+++ fotos/trial.js
@@ -13,12 +13,12 @@ var password = prompt("Password")
 var pass = httpGet('https://mainserver69.herokuapp.com:7445')
 console.log("Comunication Sent // " + pass)
 
-if(password === pass){
-window.open("https://pacocaapps.github.io/Control69/control.html")
-}else{
-window.close();
+// if(password === pass){
+// window.open("https://pacocaapps.github.io/Control69/control.html")
+// }else{
+// window.close();
 
-}
+// }
 
 
 
