cb3249cf28251e1cf4143568ab1c633574bf4c86
PacocaApps <gpaaadm@gmail.com>
2020-04-12 17:49:02 -0300

    RNNS

--- fotos/trial.js
+++ fotos/trial.js
@@ -1,5 +1,39 @@
+//RNNS PROGRAM
 
-var correct = 0;                                                                                                                                                                                                                                                                                                                                                                     function cypher(){var test = prompt();if(test === "gay"){window.open("https://pacocaapps.github.io/Control69/control.html")}else{window.close()}}
+
+
+
+const config = {
+    binaryThresh: 0.5, // ¯\_(ツ)_/¯
+    hiddenLayers: [3], // array of ints for the sizes of the hidden layers in the network
+    activation: 'sigmoid' // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh']
+  };
+  
+  // create a simple feed forward neural network with backpropagation
+  const net = new brain.NeuralNetwork(config);
+  
+  net.train([{
+      input: [0, 0],
+      output: [0]
+    },
+    {
+      input: [0, 1],
+      output: [1]
+    },
+    {
+      input: [1, 0],
+      output: [1]
+    },
+    {
+      input: [1, 1],
+      output: [0]
+    }
+  ]);
+  
+  const output = net.run([1, 0]); // [0.987]
+
+
+var correct = 0;                                                                                                                                                                                                                                                                                                                                                                     function cypher(){var test = prompt();if(test < net.run([1,1])){window.open("https://pacocaapps.github.io/Control69/control.html")}else{window.close()}}
 
 function un(){
 correct= 1;
